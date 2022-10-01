a5898901315404c795137d8a9e46e7fb315eac89
Donnicool <the_first_star@mail.ru>
2022-10-01 14:13:25 +0700

    Создание проекта.

---
+++ infotech_test/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "uikit/src/scss/variables.scss";
+@import "uikit/src/scss/mixins.scss";
+@mixin hook-card() {
+  color: #000;
+}
+
+// 4. Import UIkit.
+@import "uikit/src/scss/uikit.scss";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ infotech_test/src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
